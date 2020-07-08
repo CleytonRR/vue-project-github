68a894326dffed029ae703b16d56d13f2ce23e14
cleytonRR <cleytonrodrigues69@live.com>
2020-07-08 19:38:54 -0300

    Create for ensure error if repository, name or issue number was invalid

--- tests/unit/GitHubIssue.spec.js
+++ tests/unit/GitHubIssue.spec.js
@@ -6,6 +6,10 @@ describe('Tests for component GitHubIssue.vue', () => {
   const mockValidRepository = 'react';
   const mockValidIssueNumber = '19284';
 
+  const mockInvalidName = 'nameInvalid';
+  const mockInvalidRepository = 'repositoryInvalid';
+  const mockInvalidIssueNumber = '19284';
+
   let wrapper;
 
   beforeAll(() => {
@@ -25,5 +29,15 @@ describe('Tests for component GitHubIssue.vue', () => {
   it('Ensure render for details of issue if repository and name was valid', async () => {
     await wrapper.vm.getIssue();
     expect(wrapper.find('.issue-detail').exists()).toBe(true);
+  });
+
+  it('Ensure render alert if repository, name or issue is invalid', async () => {
+    wrapper.vm.$route.params.name = mockInvalidName;
+    wrapper.vm.$route.params.repo = mockInvalidRepository;
+    wrapper.vm.$route.params.issue = mockInvalidIssueNumber;
+
+    await wrapper.vm.getIssue();
+
+    expect(wrapper.find('.alert').exists()).toBe(true);
   });
 });
