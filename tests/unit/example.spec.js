599dd012556bd388806a8d3761f6693c774f0e84
cleytonRR <cleytonrodrigues69@live.com>
2020-07-08 16:02:19 -0300

    Added test to ensure return of title and issue number when name and repository exists

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,18 +1,30 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, RouterLinkStub } from '@vue/test-utils';
 import GitHubIssues from '@/components/GitHubIssues.vue';
 
 describe('Tests for component GitHubIssues.vue', () => {
   const mockFailName = 'failName';
   const mockFailRepository = 'FailRepository';
+  const mockValidName = 'facebook';
+  const mockValidRepository = 'react';
   let wrapper;
 
   beforeAll(() => {
-    wrapper = shallowMount(GitHubIssues);
+    wrapper = shallowMount(GitHubIssues, {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
   });
 
   it('Render alert error if name or repository not exists', async () => {
     wrapper.setData({ username: mockFailName, repository: mockFailRepository });
     await wrapper.vm.getIssues();
     expect(wrapper.find('.alert').exists()).toBe(true);
+  });
+
+  it('Render table with title and issue number when name and repository exists', async () => {
+    wrapper.setData({ username: mockValidName, repository: mockValidRepository });
+    await wrapper.vm.getIssues();
+    expect(wrapper.find('.title-issue').exists()).toBe(true);
   });
 });
